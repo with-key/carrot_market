d4eab04e2ff594946126154d502e20fa0e3e783c
with-key <rem9238@kakao.com>
2022-03-02 02:27:17 +0900

    modify: set router :: 로그인 후 메인 페이지로 이동 하는 로직 추가

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "@components/button";
 import Input from "@components/input";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
+import { useRouter } from "next/router";
 
 interface EnterForm {
   email?: string;
@@ -20,6 +21,7 @@ interface MutationResult {
 }
 
 const Enter: NextPage = () => {
+  const router = useRouter();
   const [enter, { loading, data, error }] =
     useMutation<MutationResult>("/api/users/enter");
   const [confirmToken, { loading: tokenLoading, data: tokenData }] =
@@ -44,7 +46,15 @@ const Enter: NextPage = () => {
     if (tokenLoading) return;
     confirmToken(validForm);
   };
-  console.log(loading);
+
+  console.log("tokenData", tokenData);
+
+  useEffect(() => {
+    if (tokenData?.ok) {
+      router.push("/");
+    }
+  }, [tokenData, router]);
+
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
