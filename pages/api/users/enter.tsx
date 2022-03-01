910ef31be422f1855605e7fbd47c34bef3ca510d
with-key <rem9238@kakao.com>
2022-03-02 02:27:58 +0900

    modify: simple code :: 핸드폰 번호 타입 수정 (모델), 불필요한 코드 삭제

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,11 @@
-import mail from "@sendgrid/mail";
+// import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
-mail.setApiKey(process.env.SENDGRID_KEY!);
-const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+// mail.setApiKey(process.env.SENDGRID_KEY!);
+// const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -14,7 +14,9 @@ async function handler(
   const { phone, email } = req.body;
   const user = phone ? { phone: +phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
+
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  // upsert
   const token = await client.token.create({
     data: {
       payload,
