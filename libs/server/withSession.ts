ee407acd129db376b54905597ea17831627a3ff8
with-key <rem9238@kakao.com>
2022-03-02 02:26:42 +0900

    add: helper function :: Iron seesion 랩핑 함수 추가

---
+++ libs/server/withSession.ts
@@ -0,0 +1,20 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+console.log("process.env.SESSION_PASSWORD", process.env.SESSION_PASSWORD);
+
+const cookiesOptions = {
+  cookieName: "carrotsessions",
+  password: process.env.SESSION_PASSWORD!, // 초기 타입이 string | undefined 이기 때문에 type assertion을 한다.
+};
+
+export function withApiSession(fn: any) {
+  return withIronSessionApiRoute(fn, cookiesOptions);
+}
