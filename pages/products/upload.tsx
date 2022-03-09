e9cb76795add04f79a30e60ed3631fd2306172b7
with-key <rem9238@kakao.com>
2022-03-10 01:09:44 +0900

    add: Product UI 구현

--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -3,11 +3,32 @@ import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+
+type UploadProductForm = {
+  name: string;
+  price: number;
+  description: string;
+};
 
 const Upload: NextPage = () => {
+  const { register, handleSubmit } = useForm<UploadProductForm>({
+    defaultValues: {
+      name: "",
+      price: 0,
+      description: "",
+    },
+  });
+  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+
+  const ovValid = (data: UploadProductForm) => {
+    console.log(data);
+  };
+
   return (
     <Layout canGoBack title="Upload Product">
-      <form className="p-4 space-y-4">
+      <form className="p-4 space-y-4" onSubmit={handleSubmit(ovValid)}>
         <div>
           <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
             <svg
@@ -27,16 +48,26 @@ const Upload: NextPage = () => {
             <input className="hidden" type="file" />
           </label>
         </div>
-        <Input required label="Name" name="name" type="text" />
         <Input
           required
-          label="Price"
-          placeholder="0.00"
-          name="price"
+          register={register("name")}
+          label="Name"
+          name="name"
           type="text"
+        />
+        <Input
+          required
+          register={register("price")}
+          label="Price"
+          name="price"
+          type="number"
           kind="price"
         />
-        <TextArea name="description" label="Description" />
+        <TextArea
+          register={register("description")}
+          name="description"
+          label="Description"
+        />
         <Button text="Upload item" />
       </form>
     </Layout>
